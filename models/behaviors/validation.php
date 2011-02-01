59b965b2b8cab533c279053db6374a9d04f57991
tbsmcd <tbsmcd@gmail.com>
2011-02-02 01:38:35 +0900

    fix something

--- models/behaviors/validation.php
+++ models/behaviors/validation.php
@@ -2,18 +2,21 @@
 class ValidationBehavior extends ModelBehavior {
 	function beforeValidate(&$model) {
 		$model->validate['recaptcha_response_field'] = array(
-			'confirmReCaptcha' => array(
-				'rule' => array('confirmReCaptcha', 'recaptcha_challenge_field'),
+			'checkRecaptcha' => array(
+				'rule' => array('checkRecaptcha', 'recaptcha_challenge_field'),
 				'message' => 'You did not enter the words correctly. Please try again.',
 			),
 		);
 	}
 
-	function confirmReCaptcha(&$model , $data, $target) {
+	function checkRecaptcha(&$model, $data, $target) {
 		App::import('Vendor', 'RecaptchaPlugin.recaptchalib');
 		Configure::load('RecaptchaPlugin.key');
 		$privatekey = Configure::read('Recaptcha.Private');
-		$res = recaptcha_check_answer($privatekey, $_SERVER['REMOTE_ADDR'], $model->data[$model->alias][$target], $data['recaptcha_response_field']);
+		$res = recaptcha_check_answer(
+			$privatekey, 							$_SERVER['REMOTE_ADDR'],
+			$model->data[$model->alias][$target], 	$data['recaptcha_response_field']
+		);
 		return $res->is_valid;
 	}
 }
